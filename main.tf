86189b489a14c0c8e5952db51857c2a767156d1a
Swabhiman Sarangi <49016708+swabhimansarangi@users.noreply.github.com>
2025-09-04 19:52:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0360c520857e3138f"
   instance_type          = "t2.medium"
-  key_name               = "terraformkeypair"
+  key_name               = "terrakey"
   vpc_security_group_ids = ["sg-0f1d5f2f18bb4af01"]
   tags = {
     Name = var.instance_names[count.index]
