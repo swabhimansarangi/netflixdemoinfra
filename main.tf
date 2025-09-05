f2233790dc5f6df867c4a3d543c462b57855ae4a
Swabhiman Sarangi <49016708+swabhimansarangi@users.noreply.github.com>
2025-09-05 20:19:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0360c520857e3138f"
   instance_type          = "t2.medium"
-  key_name               = "terkey"
+  key_name               = "terakey"
   vpc_security_group_ids = ["sg-05d2c71975ecf391e"]
   tags = {
     Name = var.instance_names[count.index]
